8656406c55d8b81e694fbdcb48efc5b2da58c051
Uros Radovanovic <radovanovic.uros92@gmail.com>
2015-12-28 10:10:46 +0100

    Fixed bug with product options showing on mobile devices

---
+++ js/checkWidth.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Radovanovic on 28.12.2015.
+ */
+$(document).ready(function(chkWdth) {
+    var w_height = $(window).width();
+    if (w_height < 860) {
+        $('img.dropdown-arr').removeClass('dropdown-arr-active');
+        $('img.dropdown-arr:not(".dropdown-arr-active")').closest('li').next().slideUp();
+    }
+});

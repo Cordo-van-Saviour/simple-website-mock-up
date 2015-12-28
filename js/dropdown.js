ff50d9574b8658286437487b9d2a005f0098903c
Uros Radovanovic <radovanovic.uros92@gmail.com>
2015-12-28 09:54:20 +0100

    Navigation finished, minor bug fixes

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,10 +1,9 @@
 $( document ).ready(function() {
-    //$('.toggled-options').hide();
 	$('img.dropdown-arr').on('click', function() {
 	    $(this).toggleClass('dropdown-arr-active');
 	    $(this).closest('li').next().slideDown(500);
 		$('img.dropdown-arr:not(".dropdown-arr-active")').closest('li').next().slideUp(500);
-	    })
+	    });
 });
 
 $(document).ready(function() {
